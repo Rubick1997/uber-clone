85834c10a4c5d5204d5491af18903e0846da2282
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro-2.local>
2021-11-07 13:05:50 -0800

    home screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store/store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: "https://www.freepnglogos.com/uploads/black-uber-logo-png-6.png",
+          }}
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
