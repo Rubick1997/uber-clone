38ac4728a87fbdb4375e55982dd132a033cfca7e
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro-2.local>
2021-11-24 16:15:54 -0800

    ride options card

--- components/RideOptionsCard/RideOptionsCard.tsx
+++ components/RideOptionsCard/RideOptionsCard.tsx
@@ -1,11 +1,24 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+          onPress={() => navigation.navigate("NavigateCard")}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
